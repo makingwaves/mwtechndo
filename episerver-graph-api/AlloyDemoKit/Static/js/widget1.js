e3e595755e302a16a27b33f946b7788460102a67
Łukasz Wilisowski <lukasz.wilisowski@makingwaves.com>
2019-09-19 16:40:45 +0200

    displace added

--- episerver-graph-api/AlloyDemoKit/Static/js/widget1.js
+++ episerver-graph-api/AlloyDemoKit/Static/js/widget1.js
@@ -1,24 +1,26 @@
 ﻿
-fetch("http://localhost/EpiServer.AlloyDemo.GraphAPI/account/getaccesstoken")
-    .then(response => response.json())
-    .then(accessToken => {
-        return fetch("https://graph.microsoft.com/v1.0/me/", {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-                'Content-Type': 'application/json'
-            }
-        });
+function initWidget1(accessToken) {
+    fetch("https://graph.microsoft.com/v1.0/me/", {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-Type': 'application/json'
+        }
     })
     .then(response => response.json())
     .then(graphApiJson => {
         buildItem(graphApiJson);
     });
+};
+
 
 
 function buildItem(graphApiJson) {
     var divPlaceholder = document.getElementById("placeholder1");
+
+    var divMain = document.createElement('div');
+    divMain.classList.add("mainDiv");
 
     var imageIcon = document.createElement('i');
     imageIcon.classList.add("material-icons");
@@ -42,18 +44,22 @@ function buildItem(graphApiJson) {
     var spanOfficeLocation = document.createElement('span');
     spanOfficeLocation.innerHTML = graphApiJson.officeLocation;
 
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(imageIcon);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanTitle);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanDisplayName);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanJobTitle);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanMail);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanOfficeLocation);
-    divPlaceholder.appendChild(document.createElement('br'));
-    divPlaceholder.appendChild(spanPhone);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(imageIcon);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanTitle);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanDisplayName);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanJobTitle);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanMail);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanOfficeLocation);
+    divMain.appendChild(document.createElement('br'));
+    divMain.appendChild(spanPhone);
+
+    divPlaceholder.appendChild(divMain);
+
+    initDrag(divMain);
 }
